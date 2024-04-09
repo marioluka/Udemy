72ecd604fa0f224adceb5874db102aa12f67ee7d
Mario <marioluka88@gmail.com>
2024-04-09 12:10:50 -0300

    correções

--- Aulas com exercícios/aula 15/ex/script.js
+++ Aulas com exercícios/aula 15/ex/script.js
@@ -5,7 +5,7 @@ const texto = document.getElementById("texto");
 numeroTitulo.innerHTML = numero;
 
 texto.innerHTML += `<p>Raiz quadrada: ${numero ** (1/2)}</p>`
-texto.innerHTML += `<p> ${numero} é inteiro: ${Number.isInteger()}</p>`
+texto.innerHTML += `<p> ${numero} é inteiro: ${Number.isInteger(numero)}</p>`
 texto.innerHTML += `<p>É NaN: ${Number.isNaN(numero)}</p>`
 texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`
 texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}</p>`
