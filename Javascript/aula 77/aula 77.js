2a2fa962a8acf6580dfc3dd026b8166558b41cf3
Mario <marioluka88@gmail.com>
2024-07-19 00:46:45 -0300

    Validando um CPF

---
+++ Javascript/aula 77/aula 77.js
@@ -0,0 +1,48 @@
+// 068 112 203 09
+//  0  6  8  1  1  2  2  0  3  
+// 10x 9x 8x 7x 6x 5x 4x 3x 2x += soma = resultado
+// 11 - (resultado % 11) = 1º digito
+
+function ValidaCPF(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        get: function() {
+            return cpfEnviado.replace(/\D+/g, '');
+        }
+    });
+}
+
+ValidaCPF.prototype.valida = function() {
+    if (typeof this.cpfLimpo === 'undefined') return false;
+    if (this.cpfLimpo.length !== 11) return false;
+    if (this.isSequencia()) return false;
+
+    const cpfParcial = this.cpfLimpo.slice(0,-2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+
+    const novoCPF = cpfParcial + digito1 + digito2;
+    return novoCPF === this.cpfLimpo
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        console.log(regressivo);
+        regressivo--;
+        return ac;
+    }, 0);
+
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
+};
+
+ValidaCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+}
+
+const cpf = new ValidaCPF('068.112.203-09');
+console.log(cpf.valida());
