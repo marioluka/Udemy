28c030c14e19e05bc313cfbb9cb45e7c570ddac2
Mário Luka <102702699+marioluka@users.noreply.github.com>
2024-11-25 15:19:41 -0300

    Delete aula 95 - validaCpf directory

--- aula 95 - validaCpf/src/main.js
+++
@@ -1,9 +0,0 @@
-import GeraCPF from './modules/GeraCPF.js';
-import './assets/css/style.css';
-
-
-(function() {
-    const gerador = new GeraCPF();
-    const cpfGerado = document.querySelector('.cpf-gerado');
-    cpfGerado.innerHTML = gerador.geraNovoCpf();
-})();
