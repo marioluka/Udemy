a8dbe6fa51526b13c8d940beb5e0061acbd88e97
Mário Luka <marioluka88@gmail.com>
2025-01-02 20:57:01 -0300

    Helmet e adicionando CSRF

--- Node/server.js
+++ Node/server.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
 
+const mongoose = require('mongoose');
 mongoose.connect(process.env.CONNECTIONSTRING)
   .then(() => {
     console.log("Conectei à base de dados");
@@ -14,13 +14,14 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 
-
 const routes = require("./routes.js");
 const path = require("path");
-const { middlewareGlobal } = require("./src/middlewares/middleware.js");
+const helmet = require('helmet');
+const csrf = require('csurf');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require("./src/middlewares/middleware.js");
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.resolve(__dirname, "public")));
 
 const sessionOptions = session({
@@ -40,7 +41,11 @@ app.use(flash());
 app.set("views", path.resolve(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
+app.use(csrf());
+
 app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('Pronto', () => {
--- Node/src/middlewares/middleware.js
+++ Node/src/middlewares/middleware.js
@@ -2,3 +2,14 @@ exports.middlewareGlobal = (req, res, next) => {
     res.locals.umaVariavelLocal = 'esse é o valor de uma variável local';
     next();
 };
+
+exports.checkCsrfError = (err, req, res, next) => {
+    if (err && err.code == 'EBADCSRFTOKEN') {
+        return res.send('BAD CSRF.');
+    }
+}
+
+exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+}
