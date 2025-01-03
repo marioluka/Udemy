3afcc77edec8257be20bc34b99a487ddddb66199
Mário Luka <marioluka88@gmail.com>
2025-01-02 21:10:20 -0300

    .

--- Node/src/controllers/homeController.js
+++ Node/src/controllers/homeController.js
@@ -1,6 +1,6 @@
 exports.paginaInicial = (req, res) => {
     res.render('index', {
-        tituto: 'esse é o título da pagina',
+        titulo: 'Esse é o título da pagina',
         numeros: [1,2,3,4,5,6,7,8,9]
     });
 };
